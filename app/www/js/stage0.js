416718d7744d3fd8c97c0e03d4d12bc2d59b60c6
ZWLori <lori961120@gmail.com>
2018-06-30 19:47:08 +0800

    Rename file and push to live

--- app/www/js/stage0.js
+++ app/www/js/stage0.js
@@ -1,76 +1,13 @@
+window.onload = function() {
 
-function get_attrs() {
-    if (sessionStorage.getItem("study") == 1){
-        agent_gender = $("#agent-gender").val();
-        conv_script = "N"; 
-    }
-    else if (sessionStorage.getItem("study") == 2){
-        agent_gender = $("#agent-gender").val();    
-        conv_script = $("#conv-script").val();    
-    }
-    else {
-        // used for pilot study
-        agent_gender = "N";
-        randNum = Math.floor(Math.random() * 3);
-        switch(randNum){
-            case 0: conv_script = "N"; break;
-            case 1: conv_script = "M"; break;
-            case 2: conv_script = "W"; break;
-        }
-    }
+var womanIntro = document.getElementById("womanIntro");
+var manIntro = document.getElementById("manIntro");
 
-    // store the info
-    sessionStorage.setItem("agentGender", agent_gender);
-    sessionStorage.setItem("convScript", conv_script);
+if (sessionStorage.getItem("agentGender") == "W") {
+    manIntro.style.display = "none";
+} else if (sessionStorage.getItem("agentGender") == "M") {
 
+    womanIntro.style.display = "none";
 }
 
-function selection1()
-{
-    $("#selectStudy").css("display", "none");
-    $("#selection-container").css("display", "block");
-    sessionStorage.setItem("study", 1);
-
-}
-
-function selection2()
-{
-    $("#selectStudy").css("display", "none");
-    $("#selection-container").css("display", "block");
-    $("#cs").css("display", "block");
-    sessionStorage.setItem("study", 2);
-
-}
-
-function selection3(){
-    sessionStorage.setItem("study", 3);
-    goNext();
-}
-
-function goBack()
-{
-    $("#selectStudy").css("display","block");
-    $("#selection-container").css("display", "none");
-
-}
-
-function goBack2()
-{
-    $("#selection-container").css("display","block");
-    $("#matric-container").css("display", "none");
-
-}
-
-function goNext() {
-    $("#matric-container").css("display","block");
-    $("#selection-container").css("display", "none");
-
-}
-
-
-function submitInput() {
-
-    get_attrs();
-    document.location.href = './index.html';
-
-}
+};
